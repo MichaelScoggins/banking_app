d74d31d5e310988f7519f82d2539b6c076da90b9
michaelscoggins <rhythmghosts@gmail.com>
2020-07-31 15:06:53 -0500

    completed

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ class BankAccount {
 
   deposit(amt) {
     if (amt > 0) {
-      return this.transactions.push(new Transaction(amt))
+      return this.transactions.push(new Transaction(amt, null))
     } else {
       return "Am I a joke to you?"
     }
@@ -34,7 +34,7 @@ class BankAccount {
     if (this.balance() - amt < 0) {
       return "Why don't you get a job?"
     } else if (this.balance() - amt > 0) {
-      return this.transactions.push(new Transaction(amt, payee))
+      return this.transactions.push(new Transaction(-amt, payee))
     }
   }
 }
@@ -60,64 +60,68 @@ class Transaction {
 if (typeof describe === "function") {
   describe("BankAccount", () => {
     it("Should create Bank account: account number, name, transactions", () => {
-      const bankAccount1 = new BankAccount("900-300-200", "Bob Dylan")
-      assert.equal(bankAccount1.accountNumber, "900-300-200")
-      assert.equal(bankAccount1.owner, "Bob Dylan")
-      assert.equal(bankAccount1.transactions.length, 0)
+      const michael = new BankAccount("000001", "M. Scoggins")
+      assert.equal(michael.accountNumber, "000001")
+      assert.equal(michael.owner, "M. Scoggins")
+      assert.equal(michael.transactions.length, 0)
     })
     it("Should deposit correctly", () => {
-      const myAccount = new BankAccount("001002", "Michael")
-      myAccount.deposit("Flounder", 1)
-      assert.equal(myAccount.transactions[0].amount, 1)
-      assert.equal(myAccount.transactions[0].payee, "Flounder")
-      myAccount.deposit("Sebastian", 10)
-      assert.equal(myAccount.transactions[1].amount, 10)
-      assert.equal(myAccount.transactions[1].payee, "Sebastian")
+      const michael = new BankAccount("000001", "M. Scoggins")
+      michael.deposit(100)
+      assert.equal(michael.transactions[0].amount, 100)
+      michael.deposit(50)
+      assert.equal(michael.transactions[1].amount, 50)
     })
     it("Should deduct/charge correctly", () => {
-      const newAccount = new BankAccount("001003", "Jack")
-      newAccount.balance = 51
-      newAccount.charge("Jack", 50)
-      assert.equal(newAccount.transactions[0].amount, 50)
-      assert.equal(newAccount.transactions[0].payee, "Jack")
-      newAccount.balance = 101
-      newAccount.charge("Beatrice", 100)
-      assert.equal(newAccount.transactions[1].amount, 100)
-      assert.equal(newAccount.transactions[1].payee, "Beatrice")
+      const michael = new BankAccount("000001", "M. Scoggins")
+      michael.deposit(100)
+      michael.charge("Jimmy Johns", 11)
+      assert.equal(michael.transactions[0].amount, 100)
+      assert.equal(michael.transactions[1].amount, -11)
+      assert.equal(michael.transactions[1].payee, "Jimmy Johns")
+      michael.charge("PopCopy", 8)
+      assert.equal(michael.transactions[2].amount, -8)
+      assert.equal(michael.transactions[2].payee, "PopCopy")
     })
     it("Should return correct balance after charge", () => {
-      const newAccount = new BankAccount("001003", "Jack")
-      newAccount.balance = 51
-      newAccount.charge("Jack", 50)
-      assert.equal(newAccount.balance, 1)
+      const steveBalmer = new BankAccount("001003", "S. Balmer")
+      steveBalmer.deposit(3000000)
+      steveBalmer.charge("Fine Autos, Inc.", 2500000)
+      assert.equal(steveBalmer.balance(), 500000)
+      steveBalmer.charge("Mcdonalds", 5)
+      assert.equal(steveBalmer.balance(), 499995)
     })
     it("Should prevent overdraft", () => {
       const newAccount = new BankAccount("001003", "Jack")
-      newAccount.balance = 49
-      newAccount.charge("Jack", 50)
-      assert.equal(newAccount.balance, 49)
+      newAccount.deposit(50)
+      newAccount.charge("Wal-Mart", 100)
+      assert.equal(
+        newAccount.charge("Wal-Mart", 100),
+        "Why don't you get a job?"
+      )
+      assert.equal(newAccount.balance(), 50)
     })
     it("Should not allow negative deposit", () => {
-      const myAccount = new BankAccount("001002", "Michael")
-      myAccount.balance = 20
-      myAccount.deposit("Jordan", -1)
-      assert.equal(myAccount.balance, 20)
+      const crystalAccount = new BankAccount("001002", "Crystal")
+      crystalAccount.deposit(-1)
+      assert.equal(crystalAccount.deposit(), "Am I a joke to you?")
     })
     it("Should return correct balance after multiple deposits", () => {
       const newAccount = new BankAccount("001003", "Jack")
-      newAccount.balance = 200
-      newAccount.charge("Jack", 50)
-      newAccount.charge("Jack", 50)
-      newAccount.charge("Jack", 50)
-      assert.equal(newAccount.balance, 50)
+      newAccount.deposit(200)
+      assert.equal(newAccount.balance(), 200)
+      newAccount.deposit(40)
+      newAccount.deposit(50)
+      newAccount.deposit(50)
+      assert.equal(newAccount.balance(), 340)
     })
     it("Should return correct balance after multiple charges", () => {
-      const newAccount = new BankAccount("001003", "Jack")
-      newAccount.balance = 200
-      newAccount.deposit("Jack", 50)
-      newAccount.deposit("Jack", 50)
-      newAccount.deposit("Jack", 50)
-      assert.equal(newAccount.balance, 350)
+      const michael = new BankAccount("000001", "M. Scoggins")
+      michael.deposit(900)
+      michael.charge("Vandelay Industries", 150)
+      michael.charge("Crutchfield Audio", 450)
+      michael.charge("24hr Fitness", 30)
+      assert.equal(michael.balance(), 270)
     })
   })
 }
